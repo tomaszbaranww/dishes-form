f4ef5897371c55625b4c03b141885687e488d210
PanWidmo <tomasz.baranww@gmail.com>
2023-05-15 18:49:10 +0200

    Create FormField

---
+++ src/components/FormField.js
@@ -0,0 +1,22 @@
+import styles from 'styles/FormField.module.scss';
+
+export const FormField = ({ label, error, register, id, name, type, options }) => (
+    <div className={styles.formField}>
+        <label htmlFor={id}>{label}</label>
+        {(type === 'text' || type === 'number') && <input id={id} type={type} {...register(name)} />}
+        {type === 'select' && (
+            <select id={id} {...register(name)}>
+                <option value="" hidden>
+                    Select dish type
+                </option>
+
+                {options.map((dish) => (
+                    <option key={dish} value={dish}>
+                        {dish}
+                    </option>
+                ))}
+            </select>
+        )}
+        {error && <span className={styles.errorMsg}>{error}</span>}
+    </div>
+);
